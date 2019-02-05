05a368c2b7ff3d32441188d136e7224357080a94
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2019-02-05 13:21:55 -0200

    Configurando arquivos estaticos

--- config/server.js
+++ config/server.js
@@ -1,15 +1,19 @@
 let express = require('express');
 let consign = require('consign');
 let bodyParser = require('body-parser');
+let {check, validationResult} = require('express-validator/check');
 
 let app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+app.set('check', check);
+app.set('validationResult', validationResult);
 
 app.use("/jquery",express.static('./node_modules/jquery'));
-app.use("/popper", express.static('./node_modules/popper'));
-app.use("/bootstrap",express.static('./node_modules/bootstrap'));
+app.use("/popper", express.static('./node_modules/popper.js'));
+app.use("/bootstrap/css",express.static('./node_modules/bootstrap/dist/css'));
+app.use("/bootstrap/js",express.static('./node_modules/bootstrap/dist/js'));
 app.use('/public', express.static('./app/public'));
 
 app.use(bodyParser.urlencoded({extended: true}));
