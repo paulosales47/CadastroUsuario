d43473924cf2609b7712604a7474411a1e735c0b
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2019-02-09 09:30:07 -0200

    Corrigindo rota de redirecionamento quando ocorre perda da sessão

--- app/routes/usuario.js
+++ app/routes/usuario.js
@@ -10,42 +10,42 @@ module.exports = function(aplicacao){
         if(requisicao.session.autenticado)
             aplicacao.app.controllers.usuario.novo(aplicacao, resposta);
         else
-            resposta.redirect('login/index');
+            resposta.redirect('/login');
     });
 
     aplicacao.get('/alterar', function(requisicao, resposta){
         if(requisicao.session.autenticado)
             aplicacao.app.controllers.usuario.alterar(aplicacao, requisicao, resposta);
         else
-            resposta.redirect('login/index');
+            resposta.redirect('/login');
     });
 
     aplicacao.get('/detalhe', function(requisicao, resposta){
         if(requisicao.session.autenticado)
             aplicacao.app.controllers.usuario.detalhe(aplicacao, requisicao, resposta);
         else
-            resposta.redirect('login/index');
+            resposta.redirect('/login');
     });
 
     aplicacao.get('/excluir', function(requisicao, resposta){
         if(requisicao.session.autenticado)
             aplicacao.app.controllers.usuario.excluir(aplicacao, requisicao, resposta);
         else
-            resposta.redirect('login/index');
+            resposta.redirect('/login');
     });
 
     aplicacao.post('/cadastrarUsuario', function(requisicao, resposta){
         if(requisicao.session.auntenticado)
             aplicacao.app.controllers.usuario.cadastrarUsuario(aplicacao, requisicao, resposta);
         else
-            resposta.redirect('login/index');
+            resposta.redirect('/login');
     });
 
     aplicacao.post('/atualizarUsuario', function(requisicao, resposta){
         if(requisicao.session.autenticado)
             aplicacao.app.controllers.usuario.atualizarUsuario(aplicacao, requisicao, resposta);
         else
-            resposta.redirect('login/index');
+            resposta.redirect('/login');
     });
 
 }
