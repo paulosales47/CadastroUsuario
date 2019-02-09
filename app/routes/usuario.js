333ef85f974e69f654ab9dd7672a33581577d909
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2019-02-09 05:56:35 -0200

    Verificando dados da sessão para permitir acesso as paginas da aplicação

--- app/routes/usuario.js
+++ app/routes/usuario.js
@@ -1,30 +1,51 @@
 module.exports = function(aplicacao){
     aplicacao.get('/usuario', function(requisicao, resposta){
-        aplicacao.app.controllers.usuario.index(aplicacao, resposta);
+        if(requisicao.session.autenticado)
+            aplicacao.app.controllers.usuario.index(aplicacao, resposta);
+        else
+            resposta.redirect('/login');
     });
 
     aplicacao.get('/novo', function(requisicao, resposta){
-        aplicacao.app.controllers.usuario.novo(aplicacao, resposta);
+        if(requisicao.session.autenticado)
+            aplicacao.app.controllers.usuario.novo(aplicacao, resposta);
+        else
+            resposta.redirect('login/index');
     });
 
     aplicacao.get('/alterar', function(requisicao, resposta){
-        aplicacao.app.controllers.usuario.alterar(aplicacao, requisicao, resposta);
+        if(requisicao.session.autenticado)
+            aplicacao.app.controllers.usuario.alterar(aplicacao, requisicao, resposta);
+        else
+            resposta.redirect('login/index');
     });
 
     aplicacao.get('/detalhe', function(requisicao, resposta){
-        aplicacao.app.controllers.usuario.detalhe(aplicacao, requisicao, resposta);
+        if(requisicao.session.autenticado)
+            aplicacao.app.controllers.usuario.detalhe(aplicacao, requisicao, resposta);
+        else
+            resposta.redirect('login/index');
     });
 
     aplicacao.get('/excluir', function(requisicao, resposta){
-        aplicacao.app.controllers.usuario.excluir(aplicacao, requisicao, resposta);
+        if(requisicao.session.autenticado)
+            aplicacao.app.controllers.usuario.excluir(aplicacao, requisicao, resposta);
+        else
+            resposta.redirect('login/index');
     });
 
     aplicacao.post('/cadastrarUsuario', function(requisicao, resposta){
-        aplicacao.app.controllers.usuario.cadastrarUsuario(aplicacao, requisicao, resposta);
+        if(requisicao.session.auntenticado)
+            aplicacao.app.controllers.usuario.cadastrarUsuario(aplicacao, requisicao, resposta);
+        else
+            resposta.redirect('login/index');
     });
 
     aplicacao.post('/atualizarUsuario', function(requisicao, resposta){
-        aplicacao.app.controllers.usuario.atualizarUsuario(aplicacao, requisicao, resposta);
+        if(requisicao.session.autenticado)
+            aplicacao.app.controllers.usuario.atualizarUsuario(aplicacao, requisicao, resposta);
+        else
+            resposta.redirect('login/index');
     });
 
 }
