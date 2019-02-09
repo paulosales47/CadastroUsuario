6710c3cade3f8013882dfe31bfc885b9b99bbf14
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2019-02-09 05:53:24 -0200

    Corrigindo problema na criação da sessão

--- config/server.js
+++ config/server.js
@@ -1,3 +1,4 @@
+let secret = ',3[zcuY%ZlZmbO;MN{U5GIUN(]AJ}hQ*CJhe45K^IjhkzWx#N)jT}5i@zi+s*Db)';
 let express = require('express');
 let consign = require('consign');
 let bodyParser = require('body-parser');
@@ -9,12 +10,6 @@ app.set('view engine', 'ejs');
 app.set('views', './app/views');
 app.set('check', check);
 app.set('validationResult', validationResult);
-app.set(expressSession({
-    secret: ',3[zcuY%ZlZmbO;MN{U5GIUN(]AJ}hQ*CJhe45K^IjhkzWx#N)jT}5i@zi+s*Db)',
-    resave: false,
-    saveUninitialized: false
-}));
-
 
 app.use("/jquery",express.static('./node_modules/jquery'));
 app.use("/popper", express.static('./node_modules/popper.js'));
@@ -22,7 +17,7 @@ app.use("/bootstrap/css",express.static('./node_modules/bootstrap/dist/css'));
 app.use("/bootstrap/js",express.static('./node_modules/bootstrap/dist/js'));
 app.use("/icons", express.static('./node_modules/material-design-icons'))
 app.use('/public', express.static('./app/public'));
-
+app.use(expressSession({ secret: secret, cookie: { maxAge: 1200000}, resave: false, saveUninitialized: false}));
 app.use(bodyParser.urlencoded({extended: true}));
 
 consign()
